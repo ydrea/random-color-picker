eddc00c69e349433f68138c531fd6216cc7b24a7
ydrea <andrija.wrld@gmail.com>
2021-06-21 01:55:21 +0200

    hm

--- src/App.js
+++ src/App.js
@@ -11,15 +11,13 @@ import axios from "axios";
 
 //main
 function App() {
-  const [color, colorSet] = useState('');
-  const [text, textSet] = useState([]);
-const url = `https://www.colr.org/json/color/random`
+  const [color, colorSet] = useState("");
+  const url = "https://www.colr.org/json/color/random";
 
-useEffect(() => {
-  }, [color]);
-  
-const getEm = () =>
-{ axios
+  useEffect(() => {}, [color]);
+
+  const getEm = () => {
+    axios
       .get(url)
       .then((rez) => {
         console.log(rez);
@@ -28,27 +26,55 @@ const getEm = () =>
       .catch((err) => {
         console.log(err);
       });
-  }
-console.log(color);
+  };
+  console.log(color);
 
   //display
   return (
     <div className="App">
-      <header className="App-header">fetched</header>
-      <Button
-      onClick={() => getEm()}
-      >
+      <header className="App-header">axiozed</header>
+      <Button style={{ background: `#` + `${color}` }} onClick={() => getEm()}>
         color
       </Button>
-{color.new_color}
-      <input
-        type="text"
-        value={text}
-        onChange={(event) => textSet(event.target.value)}
-      />
-      <button>Click me</button>
+      <List color={color} />
     </div>
   );
 }
 
 export default App;
+
+export const List = ({ color }) => {
+  const [text, textSet] = useState("");
+  const [colorList, colorListSet] = useState([]);
+
+  const isHex = () => {
+    let re = /[0-9A-Fa-f]{6}/g;
+    let inputString = "AABBCC";
+
+    if (!re.test(inputString)) {
+      alert("invalid hex");
+    } else {
+      return true;
+    }
+    re.lastIndex = 0;
+  };
+
+  const isIncluded = () => {
+    if (!_.includes([colorList], text)) return true;
+  };
+
+  const ifSubmit = () => {
+    if (isHex && isIncluded) return true;
+  };
+  return (
+    <div>
+      <p>{[...text, color.new_color]}</p>
+      <input
+        type="text"
+        value={text}
+        onChange={(event) => textSet(event.target.value)}
+      />
+      <button disabled={!ifSubmit}>Color me</button>
+    </div>
+  );
+};
