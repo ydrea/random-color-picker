36860a9c3017a3c74c64f0f6e195ab21ac1ea903
ydrea <andrija.wrld@gmail.com>
2021-06-20 22:33:45 +0200

    off

--- src/App.js
+++ src/App.js
@@ -6,36 +6,35 @@ import { Button } from "@material-ui/core";
 import { useState, useEffect } from "react";
 import _ from "lodash";
 //components
-import List from "./List";
 
 //main
 function App() {
   const [color, colorSet] = useState([]);
   const [text, textSet] = useState([]);
 
-  useEffect(
-    function fetchIt() {
-      async function getEm() {
-        const rez = await fetch(`https://www.colr.org/json/color/random`);
-        const json = await rez.json();
-        // colorSet(json);
-        console.log(json);
-        //de-objectify
-        _.toArray(json);
-        colorSet(json);
-        console.log(color);
-      }
-      getEm();
-    },
-    []
-  );
+  useEffect(function fetchIt() {
+    async function getEm() {
+      const rez = await fetch(`https://www.colr.org/json/color/random`);
+      const json = await rez.json();
+      // colorSet(json);
+      console.log(json);
+      //de-objectify
+      const jsn = _.toArray(json);
+      console.log(jsn);
+      colorSet(jsn);
+      console.log(color);
+    }
+    getEm();
+  }, []);
 
   //display
   return (
     <div className="App">
       <header className="App-header">fetched</header>
-      <Button onClick={() => colorSet()}>{color.new_color}</Button>
-      <List color={color} />
+      <Button 
+      //onClick={() => colorSet(color[6])}
+      >{color[6]}</Button>
+
       <input
         type="text"
         value={text}
