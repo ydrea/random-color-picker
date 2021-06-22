65ff6f800582c18c7401bce78a25076d30e39279
ydrea <andrija.wrld@gmail.com>
2021-06-22 10:42:51 +0200

    bojica teksta

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const [color, colorSet] = useState([]);
   const url = `https://www.colr.org/json/color/random?query&timestamp=${new Date().getTime()}`;
 
-//api call
+  //api call
   const getEm = () => {
     axios
       .get(url)
@@ -32,9 +32,7 @@ function App() {
   //add color to list
   const addColorToList = (color) => {
     let copy = [...colorList];
-    copy = [...copy, 
-      { id: colorList.length + 1, color, complete: false }
-    ];
+    copy = [...copy, { id: colorList.length + 1, color, complete: false }];
     colorListSet(copy);
     console.log(colorList);
   };
@@ -50,30 +48,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">axios api</header>
-      <Button
-        style={{ background: `#${color}` }}
-        onClick={() => handleClick()}
-      >
+      <Button style={{ color: `#${color}` }} onClick={() => handleClick()}>
         {color}
       </Button>
       <DragDropContext>
         <Droppable droppableId="characters">
-          {provided => (
-          <div
-            className="characters"
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-          >
-            {colorList.map((i, index) => (
-              <Draggable key={i.id} draggableId={'id'} index={index}>
-                {(provided =>
-                <p ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}> {i.color} </p>
-                )}
-              </Draggable>
-            ))}
+          {(provided) => (
+            <div
+              className="characters"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {colorList.map((i, index) => (
+                <Draggable key={i.id} draggableId={`a`} index={index}>
+                  {(provided) => (
+                    <p
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {" "}
+                      {i.color}{" "}
+                    </p>
+                  )}
+                </Draggable>
+              ))}
 
-      {provided.placeholder}
-          </div>
+              {provided.placeholder}
+            </div>
           )}
         </Droppable>
       </DragDropContext>
