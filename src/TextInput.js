7895187e6cfa460bb2da809e508ed0db2cd600bc
ydrea <andrija.wrld@gmail.com>
2021-06-23 21:03:53 +0200

    maybe

--- src/TextInput.js
+++ src/TextInput.js
@@ -1,39 +1,73 @@
 import _ from "lodash";
 import React, { useState } from "react";
+import { useFormik } from "formik";
+
 
 export const TextInput = ({
   color,
   colorSet,
-  text,
+  // text,
   textSet,
   addToList,
   colorList,
 }) => {
-  
-  //do it
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    colorSet(text);
-    addToList(color);
-    textSet("");
-  };
+//validate
+const validate = (values) => {
+  const errors = {};
 
-  const handleChange = (e) => {
-    textSet(e.currentTarget.value);
-  };
+  if (_.includes([colorList], values.text)) {
+    errors.values.text = "Previously included";
+  } else if (
+    !(
+      typeof values.text === "string" &&
+      values.text.length === 6 &&
+      !isNaN(Number("0x" + values.text))
+    )
+  ) {
+    errors.text = "Invalid hex color";
+  }
+  return errors;
+};
+
+  //formik
+  const formik = useFormik({
+    initialValues: {
+      text: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      // e.preventDefault();
+      colorSet(values.text);
+      addToList(color);
+      textSet("");
+    },
+  });
+
+  //do it
+  // const formik.handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   colorSet(text);
+  //   addToList(color);
+  //   textSet("");
+  // };
+
+  // const formik.handleChange = (e) => {
+  //   textSet(e.currentTarget.value);
+  // };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="text">Add a Hex</label>
         <input
-          value={text}
-          type="text"
+          id="text"
           name="text"
-          onChange={handleChange}
-          placeholder="enter a hex..."
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.text}
         />
-        <button type="submit">Submit</button>
 
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
